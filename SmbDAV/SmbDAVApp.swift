c8f5eb84bf0a35e99481cb40c6363748fbe8381e
FaiChou <faichou.zh@gmail.com>
2023-12-28 22:02:54 +0800

    Initial Commit

---
+++ SmbDAV/SmbDAVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmbDAVApp.swift
+//  SmbDAV
+//
+//  Created by 周辉 on 2023/12/28.
+//
+
+import SwiftUI
+
+@main
+struct SmbDAVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
